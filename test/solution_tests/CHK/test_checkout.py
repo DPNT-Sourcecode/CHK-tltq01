7ffba4ac5283fe43c5a386ba4c7fc9ede27fa61d
sbx_user1051 <sbx_user1051@169.254.118.157>
2020-11-08 22:46:37 +0000

    Sun Nov 08 22:46:37 UTC 2020

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -6,6 +6,22 @@ class TestCheckout(unittest.TestCase):
     def test_single_item(self):
         self.assertEqual(checkout("A"), 50)
 
+    def test_bad_input(self):
+        self.assertEqual(checkout(-1), -1)
+
+    def test_multiple_items(self):
+        self.assertEqual(checkout("ABCD"), 115)
+
+    def test_special_offers(self):
+        self.assertEqual(checkout("AAA"), 130)
+
+    def test_multiple_special_offers(self):
+        self.assertEqual(checkout("AAABB"), 175)
+
+    def test_special_offers_plus_one(self):
+        self.assertEqual(checkout("AAAA"), 180)
+
 
 if __name__ == "__main__":
     unittest.main()
+
