d4af24b21003bc2a19e6d6d84b405f46b0ad0b6b
sbx_user1051 <sbx_user1051@169.254.118.157>
2020-11-08 22:49:37 +0000

    Sun Nov 08 22:49:37 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,22 @@
+prices = {
+    "A": 50,
+    "B": 30,
+    "C": 20,
+    "D": 15
+}
 
+special_offers = {
+    "A": (3, 130),
+    "B": (2, 45)
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+
 def checkout(skus):
-    raise NotImplementedError()
+    cart = {}
+
+    for s in skus:
+        cart.setdefault(s, 0)
+
