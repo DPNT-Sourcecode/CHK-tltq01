0b1fa80352589884555dad84f86f8c1a48adbf79
sbx_user1051 <sbx_user1051@169.254.40.77>
2020-11-18 20:25:45 +0000

    Wed Nov 18 20:25:45 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -32,55 +32,19 @@ special_offers = [
 ]
 
 
-class LineItem:
-    def __init__(self, price, count):
-        self.price = price
-        self.count = count
-        self.discount = 0
-
-    def subtotal(self):
-        return self.price * self.count - self.discount
-
-
-def prepare_cart(skus: str) -> dict:
-    cart = dict()
-    for s in skus:
-        if s not in cart.keys():
-            cart[s] = LineItem(prices[s], 1)
-    else:
-        cart[s].count += 1
-
-    return cart
-
-
-def get_max_discount(cart: dict) -> int:
-    best_discount = -1
-
-    if "E" in cart.keys() and cart["E"] > 2 and "B" in cart.keys() and cart["B"] > 0:
-
-    for offer in sorted(special_offers, key=lambda offer: offer["priority"]):
-        item = offer["eligible_item"]
-        if item in cart.keys() and cart[item] >= offer["quantity"]:
-            discounted_item = offer["item_to_discount"]
-            if discounted_item == item:
-                pass
-            else:
-                if discounted_item in cart.keys() and cart[discounted_item] > 1:
-                    pass
-
 # noinspection PyUnusedLocal
 # skus = unicode string
 
 
 def checkout(skus: str) -> int:
+    cart = {}
     try:
-        cart = prepare_cart(skus)
+        for s in skus:
+            if s in cart.keys():
+                cart[s] += 1
+            else:
+                cart.setdefault(s, 1)
 
-        subtotals = get_item_subtotals(cart)
-
-        max_discount = get_max_discount(cart)
-
-        return total - max_discount
     except Exception as e:
         return -1
 
@@ -88,21 +52,4 @@ def checkout(skus: str) -> int:
 if __name__ == "__main__":
     print(checkout("AAABBCDEEE"))
 
-    # for item in special_offers.keys():
-    #     # Sort discounts by the greatest amount saved first
-    #     for offer in sorted(special_offers[item], key=lambda discount: discount["priority"]):
-    #         while True:
-    #             # if discount available and conditions met, add item-prices to total and apply discount
-    #             # "EEB"
-    #             if item in cart.keys() and cart[item] >= offer["quantity"]:
-    #                 subtotal = prices[item] * offer["quantity"]  # 2E * 40
-    #                 cart[item] -= offer["quantity"]  # "B"
-
-    #                 if item == offer["item_to_discount"]:
-    #                     subtotal -= offer["discount"]
-    #                 elif offer["item_to_discount"] in cart.keys() and cart[offer["item_to_discount"]] > 0:
-
-    #                 total += subtotal
-    #             else:
-    #                 break
 
