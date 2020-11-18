d31ead410a69aa688c645c1048d3a122f027614a
sbx_user1051 <sbx_user1051@169.254.85.133>
2020-11-18 00:24:59 +0000

    Wed Nov 18 00:24:59 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -55,9 +55,6 @@ def checkout(skus):
     total = 0
 
     try:
-        # if len(skus) == 0:
-        #     raise ValueError
-
         # count the numer of each item in the cart
         for s in skus:
             if s not in cart:
@@ -86,6 +83,7 @@ def checkout(skus):
 
 if __name__ == "__main__":
     print(checkout("A"))
+
 
 
 
