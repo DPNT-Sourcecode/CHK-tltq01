d931953748e043928260dac2768cf0705db009d2
sbx_user1051 <sbx_user1051@169.254.161.109>
2020-11-26 18:03:24 +0000

    Thu Nov 26 18:03:24 UTC 2020

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -77,11 +77,13 @@ class TestCheckout(unittest.TestCase):
 
 # 3Q for 80
 
+
     def test_special_offer_three_q(self):
         self.assertEqual(checkout("QQQ"), 80)
 
 
 # 3R get one Q free
+
 
     def test_special_offer_three_r(self):
         self.assertEqual(checkout("RRR"), 150)
@@ -92,6 +94,7 @@ class TestCheckout(unittest.TestCase):
 
 # 3U get one U free
 
+
     def test_special_offer_three_u(self):
         self.assertEqual(checkout("UUU"), 120)
 
@@ -101,17 +104,34 @@ class TestCheckout(unittest.TestCase):
 
 # 2V for 90
 
+
     def test_special_offer_two_v(self):
         self.assertEqual(checkout("VV"), 90)
 
 
 # 3V for 130
 
+
     def test_special_offer_three_v(self):
         self.assertEqual(checkout("VVV"), 130)
 
     def test_special_offer_a_to_z(self):
         self.assertEqual(checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), 965)
+
+    def test_group_three_z(self):
+        self.assertEqual(checkout("ZZZ"), 45)
+
+    def test_group_four_z(self):
+        self.assertEqual(checkout("ZZZZ"), 66)
+
+    def test_group_desc(self):
+        self.assertEqual(checkout("ZXYT"), 45)
+
+    def test_group_ascend_price(self):
+        self.assertEqual(checkout("XYTSZ"), 66)
+
+    def test_group_desc_price(self):
+        self.assertEqual(checkout("ZSTYX"), 66)
 
 
 class TestLoadTable(unittest.TestCase):
@@ -128,3 +148,4 @@ class TestLoadTable(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
